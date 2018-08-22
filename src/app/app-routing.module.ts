a225e3e0485aae9e473d2caa40a9a02dfc181e65
Samartha Kajoor Venkatramana <samarthakv29@gmail.com>
2018-08-22 16:54:57 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RatematchComponent } from './ratematch/ratematch.component';
+import { PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   { path: '', component: RatematchComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
